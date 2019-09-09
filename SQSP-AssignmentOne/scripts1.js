e42da93edcd84c7e2a5fba6c7e67bbc2a13f3e13
Juan Herrera <juanherrera@Juans-MacBook-Pro.local>
2019-09-09 00:38:51 -0400

    Work on assignment 1

--- SQSP-AssignmentOne/scripts1.js
+++ SQSP-AssignmentOne/scripts1.js
@@ -1,3 +1,55 @@
+//JSON Data
+var universeData = {
+	"array": [{
+		"id": "0001",
+		"skullSrc": "https://raw.githubusercontent.com/herrj636/herrj636.github.io/master/SQSP-AssignmentOne/assets/skull-1.jpg",
+		"styles": {
+			"font": {
+				"link": "https://fonts.googleapis.com/css?family=Open+Sans:400,600,700&display=swap"
+			}
+		},
+	},
+	{
+		"id": "0002",
+		"skullSrc": "/Users/juanherrera/Dropbox/herrj636.github.io/SQSP-AssignmentOne/assets/skull-2.svg",
+		"styles": {
+			"font": {
+				"link": "https://fonts.googleapis.com/css?family=Open+Sans:400,600,700&display=swap"
+			}
+		},
+	}]
+}
 
-var check = console.log("Connected to DOM");
+// Global Varibles
+var trigger = document.getElementById("trigger");
+
+//Modules
+mainUniverseLoad = () => {
+	let universe = universeData.array[0];	
+	document.getElementById("fonts").setAttribute('href',universe.styles.font.link);
+
+	hideMainUniverse = () => {
+		document.getElementById("universeOne").style.visibility = "hidden";
+	};
+};
+
+mainUniverseLoad();
+
+
+trigger.addEventListener('click', function (event) {
+  // alert('Element clicked through function!');
+hideMainUniverse();
+  UniverseLaunch = () => {
+  	let universe = document.getElementById("universeTwo")
+  	universe.style.visibility = "visible"
+  	console.log(universe.children)
+}
+
+UniverseLaunch();
+});
+
+
+
+
+
 
