4ae302306b9e7ee8bd801555a21aa1ea79d2ccbf
Juan Herrera <herrerj2@us3ketm0yd1dxjh.ketchum.com>
2019-10-29 09:44:44 -0400

    Deploy V1 of Signature generator

---
+++ Signature-Tool/copyScript.js
@@ -0,0 +1,25 @@
+  // Documentation on selecting, copying and pasting to clipboard: https://stackoverflow.com/questions/2044616/select-a-complete-table-with-javascript-to-be-copied-to-clipboard
+
+  //Select and copy the signature selected
+  function selectElementContents(el) {
+        var body = document.body, range, sel;
+        if (document.createRange && window.getSelection) {
+            range = document.createRange();
+            sel = window.getSelection();
+            sel.removeAllRanges();
+            try {
+                range.selectNodeContents(el);
+                sel.addRange(range);
+            } catch (e) {
+                range.selectNode(el);
+                sel.addRange(range);
+            }
+            document.execCommand("copy");
+
+        } else if (body.createTextRange) {
+            range = body.createTextRange();
+            range.moveToElementText(el);
+            range.select();
+            range.execCommand("Copy");
+        }
+    }
