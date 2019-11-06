a89759e9e04d6a7847359fc5365dada11c893f79
Juan Herrera <herrerj2@us3ketm0yd1dxjh.ketchum.com>
2019-11-06 13:29:27 -0500

    Updates to javascript code

--- Signature-Tool/signatureGenerator.js
+++ Signature-Tool/signatureGenerator.js
@@ -1,47 +1,100 @@
-
 console.log("I'm here!");
 
 const data = document.forms['userData'];
 const errorElement = document.getElementById('error');
 
-	var name = data.querySelector('input[tabindex="1"]').value;
-	var title = data.querySelector('input[tabindex="2"]').value;
-	var directNum = data.querySelector('input[tabindex="3"]').value;
-	var mobileNum = data.querySelector('input[tabindex="4"]').value;
-	var webexURL = data.querySelector('input[tabindex="5"]').value;
-	var linkedinURL = data.querySelector('input[tabindex="6"]').value;
-	var pronoun = data.querySelector('select[tabindex="7"]').value;
 
+data.addEventListener("submit", (e) => {
+    e.preventDefault();
+    validateForm();
+});
 
-formvalidate = () => {
-	console.log("I'm here now!");
-	console.log(form)
+validateForm = () => {
+    let messages = [];
+    var pronoun = data.querySelector('select[tabindex="7"]').value;
+    let validateCode = false 
+
+    if (pronoun == "pronoun") {
+        messages.push('Please choose a valid pronoun option');
+    }
+
+    if (messages.length > 0) {
+        errorElement.innerText = messages.join(', ');
+        console.log(messages.length);
+        return false
+    } else if (messages.length == 0) {
+    	errorElement.innerText = messages;
+    	console.log(messages.length)
+    	createSignature();
+        return true
+    }
 }
 
 
-data.addEventListener("submit", (e) => {
-	e.preventDefault();
-	let messages = []
+createSignature = () => {
+    console.log("I'm here now!");
+    var name = data.querySelector('input[tabindex="1"]').value;
+    var title = data.querySelector('input[tabindex="2"]').value;
+    var directNum = data.querySelector('input[tabindex="3"]').value;
+    var mobileNum = data.querySelector('input[tabindex="4"]').value;
+    var webexURL = data.querySelector('input[tabindex="5"]').value;
+    var linkedinURL = data.querySelector('input[tabindex="6"]').value;
+    var pronoun = data.querySelector('select[tabindex="7"]').value;
 
-	console.log(name);
-	console.log(title);
-	console.log(directNum);
-	console.log(mobileNum);
-	console.log(webexURL);
-	console.log(linkedinURL);
-	console.log(pronoun);
+    // console.log(name);
+    // console.log(title);
+    // console.log(directNum);
+    // console.log(mobileNum);
+    // console.log(webexURL);
+    // console.log(linkedinURL);
+    // console.log(pronoun);
 
-	if (pronoun === 'null') {
-		messages.push('Please choose a valid pronoun option')
-	}
+    document.getElementById("core-sig-container").style.visibility = "visible";
+    
+    var partOne = document.getElementById("firstLine").insertAdjacentHTML('afterbegin', '<strong style="font-size:16px; color: #303030; line-height: 18px; height: 1px;">' + name + '</strong>');
+    console.log(partOne);
 
-	if (messages.length > 0) {
-		errorElement.innerText = messages.join(', ')
-	}
 
-	
 
-});
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
